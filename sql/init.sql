3b40f8271d221883d862782935f41bac940d38c9
lalyos <lalyos@yahoo.com>
2024-01-30 11:10:02 +0100

    sql added

---
+++ sql/init.sql
@@ -0,0 +1,4 @@
+create table vip (name varchar(20), age int, money int);
+insert into vip values('elon', 45, 2000 );
+insert into vip values('lorinc', 65, 200);
+insert into vip values('klara', 55, 20);
